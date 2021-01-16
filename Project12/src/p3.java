39c16a9c09bf4d209e5fc2f5f83857c185b1bfd2
mkwkw <mkkw2047@gmail.com>
2021-01-16 18:52:10 +0900

    n과m

---
+++ Project12/src/p3.java
@@ -0,0 +1,18 @@
+//백준 15663
+//n과 m
+//백트래킹
+//n개에서 m개를 중복 없이 고르기, 오름차순으로 출력
+//3 1
+//4 4 2
+//4 4 2 3개의 숫자 중에 1개를 고르는 경우를 출력
+//2
+//4
+
+public class p3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
