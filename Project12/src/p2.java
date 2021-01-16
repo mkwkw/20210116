d326b57b6ea440e2574e797df3f954a0604b40b4
mkwkw <mkkw2047@gmail.com>
2021-01-16 17:44:52 +0900

    행렬

---
+++ Project12/src/p2.java
@@ -0,0 +1,15 @@
+//백준 1080
+//행렬
+//그리디
+//현재 상황에서 지금 당장 좋은 것 고르기
+//아이디어: [0][0]의 값이 목표값과 다르면 전환, [0][1]의 값이 목표값과 다르면 전환 ---
+//필요: 현재 배열 저장할 배열, 목표 배열 저장할 배열, 전환 횟수 저장할 변수, 현재 배열과 목표 배열 비교
+import java.util.*;
+public class p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
